4dc315c9a7960cb981cd55347374f31b67315605
Mendelsson Brandao <mendelsson@gmail.com>
2024-07-30 22:06:22 -0300

    Incluindo exemplo de ForEach na mesma classe

--- primeiro-projeto-java/src/ExemploForArray.java
+++ primeiro-projeto-java/src/ExemploForArray.java
@@ -6,5 +6,11 @@ public class ExemploForArray {
         for (int i=0; i<alunos.length; i++) {
             System.out.println("Aluno [" +i+ "]: " + alunos[i]);
         }
+
+        System.out.println("\n");
+
+        for (String aluno : alunos) {
+            System.out.println(aluno);
+        }
     }
 }
