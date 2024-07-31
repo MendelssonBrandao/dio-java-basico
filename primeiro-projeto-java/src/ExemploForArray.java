030ae7e943dd7891a070493508b4bbf99e5c6691
Mendelsson Brandao <mendelsson@gmail.com>
2024-07-30 21:57:32 -0300

    Criando uma classe com exemplo de comando For

---
+++ primeiro-projeto-java/src/ExemploForArray.java
@@ -0,0 +1,10 @@
+public class ExemploForArray {
+
+    public static void main(String[] args) {
+        String alunos [] = { "Felipe", "Jonas", "Julia", "Marcos"};
+
+        for (int i=0; i<alunos.length; i++) {
+            System.out.println("Aluno [" +i+ "]: " + alunos[i]);
+        }
+    }
+}
